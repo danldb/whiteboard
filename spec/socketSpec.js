81553fd941a02f629a12926502247cf0bc8022d9
DanLDB <dpbergquist>
2014-12-14 11:53:01 +0000

    failing test: first user connects

--- spec/socketSpec.js
+++ spec/socketSpec.js
@@ -7,10 +7,18 @@ var options = {
   'force new connection': true
 };
 
+var userName1 = 'Dan';
+
 describe('Socket Server', function(){
 
   it('should add a single user', function(next){
-
+    var connected = false;
+    var user1 = io.connect(socketURL, options);
+      user1.on('connect', function(){
+        connected = true;
+      });
+    expect(connected).toBe(true);
+    next();
   });
 
 });
