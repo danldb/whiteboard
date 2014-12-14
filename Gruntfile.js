4e9caa2898a1b584f695797d62c9a2c5ca7b5662
DanLDB <dpbergquist>
2014-12-14 10:34:02 +0000

    building Gruntfile: add jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,23 @@ module.exports = function(grunt) {
         forceExit: true,
       },
       all: ['spec/']
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'test/*.js', 'src/*.js', 'spec/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('default', ['jasmine_node']);
+  grunt.registerTask('default', ['jshint','jasmine_node']);
 };
